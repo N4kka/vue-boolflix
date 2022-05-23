63650cf49197a94a77ee436a60cb03fcadeb830b
Nicola Sarli <you@example.com>
2022-05-23 12:36:17 +0200

    Scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>BOOLFLIX</h1>
+    <div class="searchbar">
+      <input type="text" name="Searchbar" id="" />
+      <button>Search</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  height: 100px;
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    color: red;
+    line-height: 100px;
+    margin-left: 2rem;
+  }
+
+  .searchbar {
+    margin-right: 2rem;
+  }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <ul>
+      <li>ciao</li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  background-color: pink;
+  height: calc(100vh - 100px);
+}
+</style>
